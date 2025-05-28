e40063c60856e0931c848bcba6950613a7646a96
Emre Kılıç <emrekilic87@gmail.com>
2025-05-28 10:29:13 +0300

    footer design has been changed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,9 +2,9 @@
 
 export default function Footer() {
   return (
-    <footer className="sticky bottom-0 w-full py-4 border-t border-gray-200 bg-white">
+    <footer className="sticky bottom-0 w-full py-2 border-t border-gray-200 bg-white">
       <div className="max-w-6xl mx-auto flex flex-col items-center justify-center space-y-4">
-        <p className="text-sm text-gray-500 text-center"> © {new Date().getFullYear()} All Rights Reserved <br/> multihatstravel@gmail.com</p>
+        <p className="text-xs text-gray-500 text-center"> © {new Date().getFullYear()} All Rights Reserved <br/> multihatstravel@gmail.com</p>
       </div>
     </footer>
   );
